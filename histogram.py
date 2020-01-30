514bd73fbdb381aa7714c0adec4553b401f73f61
Jorge Gallegos <gallegosjorge403@gmail.com>
2020-01-30 10:17:53 -0800

    Added docstrings

--- histogram.py
+++ histogram.py
@@ -1,4 +1,6 @@
 def histogram(source_text):
+    """Takes in the name of the file and returns a 
+       histogram of how often words appear in the text"""
     with open(source_text, 'r') as file:
         lines = file.read()
         words = lines.split()
@@ -15,10 +17,13 @@ def histogram(source_text):
         return word_frequency
 
 def unique_words(histogram):
+    """Returns the number of unique words in the histogram"""
     return len(histogram)
 
 
 def frequency(word, histogram):
+    """Returns the number of times the word appears
+       in the histogram"""
     return histogram[word]
 
 if __name__ == "__main__":
@@ -26,4 +31,4 @@ if __name__ == "__main__":
     words = histogram(source_text)
     print(words)
     print(unique_words(words))
-    print(frequency('beyond', words))
+    print(frequency('exalted', words))
