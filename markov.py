aec8e19a9bb94d01cfbe9055eabf81168d5deb48
Jorge Gallegos <gallegosjorge403@gmail.com>
2020-03-04 17:09:34 -0800

    Finished

---
+++ markov.py
@@ -0,0 +1,56 @@
+from dictogram import Dictogram
+
+class MarkovChain:
+
+    def __init__(self, word_list):
+
+
+        #The Markov chain will be a dictionary of dictionaries
+        #Example: for "one fish two fish red fish blue fish"
+        #{"one": {fish:1}, "fish": {"two":1, "red":1, "blue":1}, "two": {"fish":1}, "red": {"fish":1}, "blue": {"fish:1"}}
+         self.markov_chain = self.build_markov(word_list)
+         self.first_word = list(self.markov_chain.keys())[0]
+         self.word_list = word_list
+
+    def build_markov(self, word_list):
+        markov_chain = {}
+
+        for i in range(len(word_list) - 1):
+            #get the current word and the word after
+            current_word = word_list[i]
+            next_word = word_list[i+1]
+
+            if current_word in markov_chain.keys(): #already there
+                #get the histogram for that word in the chain
+                histogram = markov_chain[current_word]
+                #add to count
+                histogram[next_word] = histogram.get(next_word, 0) + 1
+            else: #first entry
+                markov_chain[current_word] = Dictogram([next_word])
+
+        return markov_chain
+
+    def walk(self, num_words):
+        words = []
+        histogram = Dictogram(self.word_list)
+        next_word = histogram.sample()
+
+        words.append(next_word)
+        for i in range(num_words - 1):
+            if len(self.markov_chain) > 0:
+                next_word = histogram.sample()
+                words.append(next_word)
+
+        sentence = ' '. join(words)
+
+        return sentence
+
+    def print_chain(self):
+        for word, histogram in self.markov_chain.items():
+            print(word, histogram)
+
+
+
+markov_chain = MarkovChain(["one", "fish", "two", "fish", "red", "fish", "blue", "fish"])
+markov_chain.print_chain()
+print(markov_chain.walk(10))
